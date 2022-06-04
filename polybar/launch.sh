80de2fa69706aa9fce854599107a82e17ebd590e
Mark Nguyen <pnguyen1097@gmail.com>
2022-06-04 12:52:23 -0400

    Update polybar.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,7 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main -r &
-polybar hdmi -r &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  echo $m
+  MONITOR=$m polybar main --reload &
+done
